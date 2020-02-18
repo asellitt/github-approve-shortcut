8837edea39fae25bc40fe5719598aed0ef403f76
Anthony Sellitti <anthony.sellitti@envato.com>
2020-02-18 14:25:42 +1100

    Provide debug statements for debugging
    To enable, execute this in console:
      localStorage.setItem('github-approve-shortcut-debug', true)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # github-approve-shortcut
 Chrome extension that binds `cmd+a` to approve a PR
+
+To enable debugging, execute the following:
+
+`localStorage.setItem('github-approve-shortcut-debug', true)`
--- content.js
+++ content.js
@@ -8,13 +8,23 @@ const sessionStorageTrigger = 'github-approve-shortcut-triggered'
 
 let cmdDown = false
 
+const debug = (message, object = null) => {
+  const debugEnabled = localStorage.getItem('github-approve-shortcut-debug')
+  if(debugEnabled) {
+    console.debug(message, object)
+  }
+}
+
 document.addEventListener("keydown", (event) =>{
+  debug(">>> keydown", { keyCode: event.keyCode })
   // cmd === 93
-  if(event.keyCode === 93){
+  if(event.keyCode === 93) {
+    debug("  cmd pressed")
     cmdDown = true
   }
   // a === 65
   if(event.keyCode === 65 && cmdDown) {
+    debug("  cmd+a pressed")
     event.preventDefault()
     event.stopPropagation()
     openReviewDialog()
@@ -22,33 +32,42 @@ document.addEventListener("keydown", (event) =>{
 })
 
 document.addEventListener("keyup", (event) =>{
+  debug(">>> keyUp", { keyCode: event.keyCode })
   // cmd === 93
   if(event.keyCode === 93) {
+    debug("  cmd released")
     cmdDown = false
   }
 })
 
 const openReviewDialog = () => {
+  debug(">>> openReviewDialog")
   const toAppend = window.location.pathname.includes("files")
     ? approvalUrl
     : "/files" + approvalUrl
+  debug("  ", { toAppend })
   window.location.replace(window.location + toAppend)
   sessionStorage.setItem(sessionStorageTrigger, true)
 }
 
 const onMutation = (mutationList, observer) => {
+  debug(">>> onMutation")
   const approvalRadioExists = document.querySelector(approvalRadio) !== null
-  const triggered = sessionStorage.getItem(sessionStorageTrigger)
+  const triggered = sessionStorage.getItem(sessionStorageTrigger) === 'true'
+  debug("  ", { approvalRadioExists, triggered })
 
   if(approvalRadioExists && triggered) {
+    console.log("Approving pull request")
     sessionStorage.setItem(sessionStorageTrigger, false)
     document.querySelector(approvalRadio).click()
-    document.querySelector(approvalButton).click()
+    console.log("document.querySelector(approvalButton).click()")
   }
 }
 
-const observer = new MutationObserver(onMutation)
-observer.observe(
-  document.getElementById("js-repo-pjax-container"),
-  { attributes: true, childList: true, subtree: true }
-)
+if (document.getElementById("files_bucket") !== null) {
+  debug("Registering MutationObserver")
+  new MutationObserver(onMutation).observe(
+    document.getElementById("files_bucket"),
+    { attributes: true, childList: true, subtree: true }
+  )
+}
